d284c8aacac0410dd9820e3198d58dfa9a638203
rjfc <russelljfchan@gmail.com>
2017-03-10 13:36:21 -0500

    Removed schemas and started socket io

--- models/conversation.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose"),
-    Schema   = mongoose.Schema;
-
-var ConversationSchema = new Schema({
-   participants: [{type: Schema.Types.Objectid, ref: "User"}],
-});
-
-module.exports = mongoose.model("Conversation", ConversationSchema);
